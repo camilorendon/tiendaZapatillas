0ef1c153890690847c3cef60010671a5139bb68f
camilorendon <juancamilo.g.0512@gmail.com>
2023-10-24 18:59:29 -0500

    feat: finish api

--- src/models/client.js
+++ src/models/client.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 
-const clientSchema = new mongoose.Schema({
+const specialPriceSchema = new mongoose.Schema({
   name: String,
-  specialPrices: [
-    {
-      brand: String,
-      price: Number,
-    },
-  ],
+  brand: String,
+  price: Number,
 });
 
-module.exports = mongoose.model("Client", clientSchema);
+module.exports = mongoose.model("Client", specialPriceSchema);
--- src/routes/prices.js
+++ src/routes/prices.js
@@ -3,6 +3,28 @@ const Client = require("../models/client");
 
 const router = express.Router();
 
+router.post("/price", async (req, res) => {
+  const { userId, brand, specialPrice } = req.body;
+
+  try {
+    // Verificar si ya existe un precio especial para el usuario y la marca
+    const existingSpecialPrice = await specialPrice.findOne({ userId, brand });
+    if (existingSpecialPrice) {
+      res.status(400).json({
+        error: "There is already a special price for this user and brand.",
+      });
+      return;
+    }
+
+    const specialPriceDoc = new specialPrice({ userId, brand, specialPrice });
+    await specialPriceDoc.save();
+
+    res.json({ message: "Special price created successfully." });
+  } catch (error) {
+    res.status(500).json({ error: "Error creating the special price." });
+  }
+});
+
 // Obtener precio especial para un cliente y marca
 router.get("/price/:user_id/:name_product", async (req, res) => {
   const { id, name } = req.params;
